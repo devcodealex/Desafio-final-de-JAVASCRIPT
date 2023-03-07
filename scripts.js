05f77853543c0b4631a70c35f11d47f637689802
devcodealex <devcodealex@gmail.com>
2023-03-07 19:47:56 -0300

    consumo de api  usando fetch-async-await

--- scripts.js
+++ scripts.js
@@ -2,14 +2,20 @@ const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
 
 
-const dolar = 5.19
-const euro = 5.52
-const bitcoin = 123266.34 
 
-const convertValues = () => {
+
+const convertValues = async() => {
     const inputReais = document.getElementById('input-real').value
     const realText = document.getElementById('real-value-text')
     const currencyText = document.getElementById('currency-value-text')
+
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response=>response.json())
+
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const bitcoin = data.BTCBRL.high
+    
+    console.log(data)
 
     realText.innerHTML = new Intl.NumberFormat('pt-BR',
         { style: 'currency', currency: 'BRL' }
